230dd40f637a7559b022f4e400a8412566a39c63
Hilda Ng <hng042899@yahoo.com>
2015-01-21 12:10:40 -0800

    Completed numbers and variables

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,18 +11,25 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
-    /* WORK HERE */
-    return number;
-}
+    NSInteger numberPlusOne = number +  1;
+    NSLog(@"number(%ld) + 1 = %ld",number, (long)numberPlusOne);
 
+    return numberPlusOne;
+}
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    /* WORK HERE */
-    return number1;
+   
+    NSInteger sum = number1 + number2;
+    NSLog(@"Number1 + Number2 = %ld", (long)sum);
+    
+    return sum;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
-    /* WORK HERE */
-    return 0;
+    NSInteger remainder = dividend % divisor;
+    NSLog(@"The remainder is %ld", (long)remainder);
+    
+    return remainder;
 }
+
 
 @end
