6c6ff9deafd992c18a823b7f760ae1971bf6a553
Hilda Ng <hng042899@yahoo.com>
2015-04-15 17:19:14 -0700

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,88 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    
+    // Type (includes class and * meaning pointer to)
+    //         name         [= ...];
+    NSNumber* resultNSNumber = nil;
+    
+    double floatNumber = 2 * [number doubleValue];
+    
+    // [ClassOrInstance methodName:parameters]
+    
+    resultNSNumber = [NSNumber numberWithDouble:floatNumber];
+    
+    return resultNSNumber;
 }
+
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+ 
+    // start with an empty stack of result numbers
+    NSArray *arrayOfNumbersBetweenNumber = @[];
+    
+    NSInteger lowerNumber;
+    NSInteger higherNumber;
+    
+    // find lower and higher number and save them
+    if (number < otherNumber) {
+        lowerNumber = number;
+        higherNumber = otherNumber;
+    } else {
+        higherNumber = number;
+        lowerNumber = otherNumber;
+    }
+    
+    NSLog(@" %ld - %ld ", (long) lowerNumber, (long) higherNumber);
+    
+    
+    // set current number to the lower number, then while current number is lower than higher number:
+    for (NSInteger currentNumber = lowerNumber; currentNumber <= higherNumber;) {
+        NSLog(@"CurrentNumber: %ld",currentNumber);
+        
+        // wrap current number in an NSNumber object
+        NSNumber *curNumberObject = [NSNumber numberWithInteger:currentNumber];
+        
+        // add the current number to the stack of results
+        arrayOfNumbersBetweenNumber = [arrayOfNumbersBetweenNumber arrayByAddingObject:curNumberObject];
+        
+        // add 1 to the current number
+        currentNumber = currentNumber + 1;
+    }
+
+    return arrayOfNumbersBetweenNumber;
+    
 }
 
+
+// No pointers to:
+// NSInteger, double, char, BOOL, CGFloat, float
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    NSInteger resultNSInteger = 0;
+    NSInteger lowestSoFar = 32000;
+    
+    // for each number in the array {
+    
+    for (NSNumber *currentNumber in arrayOfNumbers) {
+  
+    
+    
+            // if it's lower than the lowest so far,
+        
+        if ([currentNumber integerValue] < lowestSoFar) {
+            lowestSoFar = [currentNumber integerValue];
+            
+        }
+    }
+    
+    
+    // then set the lowest so far after all  numbers are done to be what we return
+    resultNSInteger = lowestSoFar;
+    
+    return resultNSInteger;
 }
 
 @end
